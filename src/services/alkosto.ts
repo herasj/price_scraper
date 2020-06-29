dc10338c3fa3869fc04da5557e38b113d69be229
Juan Rambal <herasj@uninorte.edu.co>
2020-06-28 23:29:16 -0500

    feat: product links

--- src/services/alkosto.ts
+++ src/services/alkosto.ts
@@ -1,16 +1,27 @@
-import { launch, Page } from "puppeteer";
-import { writeFileSync } from "fs";
+import { IProduct } from "../interfaces/product.interface";
+import { writeFileSync, appendFileSync } from "fs";
+import { launch, Page, Browser } from "puppeteer";
+import { union } from "lodash";
+
 export class AlkostoService {
   private readonly startUrl: string;
+  private browser: Browser | null;
   private page: Page | null;
+  private data: IProduct[];
+
   constructor() {
-    this.page = null;
     this.startUrl = "https://www.alkosto.com/electro";
+    this.browser = null;
+    this.page = null;
+    this.data = [];
   }
 
   private setup = async (): Promise<void> => {
-    const browser = await launch({ headless: true,defaultViewport:{ width: 1920, height: 1080 } });
-    this.page = await browser.newPage();
+    this.browser = await launch({
+      headless: true,
+      defaultViewport: { width: 1920, height: 1080 },
+    });
+    this.page = await this.browser.newPage();
     await this.goToUrl(this.startUrl);
   };
 
@@ -19,19 +30,56 @@ export class AlkostoService {
   };
 
   private takeScreenshot = async (): Promise<void> => {
-    await this.page.screenshot({ path: `${process.cwd()}/screenshots/${Date.now()}.png` });
+    await this.page.screenshot({
+      path: `${process.cwd()}/screenshots/${Date.now()}.png`,
+    });
   };
 
   private getProducts = async () => {
-      const ul = await this.page.$('[class="products-grid last even"]')
-      writeFileSync(`${process.cwd()}/lista.txt`,ul.toString())
-      console.dir(ul)
+    await this.page.waitForSelector('[class="products-grid last even"] > li > div > a');
+    const items = await this.page.$$(
+      '[class="products-grid last even"] > li > div > a'
+    );
+    const data = [];
+    for await (let item of items) {
+      const link = await (await item.getProperty("href")).jsonValue();
+      const title = await (await item.getProperty("title")).jsonValue();
+      data.push({ link: `${link}`, title: `${title}` });
+    }
+    console.log('\n\n')
+    this.data = union(this.data, data)
+    console.dir(this.data)
+  };
+
+  private nextPage = async () => {
+    await this.page.click('[class="next i-next"]');
+    await this.getProducts();
+  };
+
+  private productInfo = async() => {
+      for await(const item of this.data) {
+        await this.page.goto(item.link, { waitUntil: "networkidle0" });
+        await this.page.waitForSelector('[class="price-old"]')
+      }
   }
   
-
   public extractData = async (): Promise<void> => {
+    const pages = [2, 3, 4, 5];
     await this.setup();
     await this.takeScreenshot();
     await this.getProducts();
+    for await (const page of pages) {
+      console.dir(page);
+      await this.nextPage();
+      console.log("endpage");
+    }
+    console.dir(this.data)
+    for (const item of this.data) {
+      appendFileSync(
+        `${process.cwd()}/lista.txt`,
+        `${item.title}\n${item.link}\n\n`
+      );
+    }
+    await this.browser.close();
   };
 }
