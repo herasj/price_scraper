f6d7c772eff236448af301a549120f8a62a889e8
Juan Rambal <herasj@uninorte.edu.co>
2020-06-29 13:56:36 -0500

    refactor: target url is now on .env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,5 +2,6 @@ import { IEnvironment } from '../interfaces/environment.interface';
 require("dotenv").config(); 
 
 export const environment: IEnvironment ={
- MONGO_URI: process.env.MONGO_URI
+ MONGO_URI: process.env.MONGO_URI,
+ TARGET_URL: process.env.TARGET_URL
 }
