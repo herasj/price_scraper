9c7255f806cbc740210b2506e43e1f2f1de6e89f
Juan Rambal <herasj@uninorte.edu.co>
2020-06-29 12:57:43 -0500

    style: format code

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Scripts
+# Price Traker
 
-> npm start.
 
-To compile and execute the script, after that you can use npm run start:prod to avoid recompiling the code
+## Usage
+
+> npm start
+
+To compile and execute the script, after that you can use **npm run start:prod** to avoid recompiling the code.
+
+----------
+
+**Please make sure to create an .env on /src , you can watch the environment schema on /src/config/environment.ts also, you have to create a screenshots folder on the project root folder**
--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ class Main {
     this.alkostoService = new AlkostoService();
   }
   start() {
-    connection.then(async () => {
-      console.log("Connected to database");
-      await this.alkostoService.extractData();
-      console.log("END");
-    })
-    .catch((error) => {
-      console.dir(`Error on db connection: ${error}`)
-    }
-    )
+    connection
+      .then(async () => {
+        console.log("Connected to database");
+        await this.alkostoService.extractData();
+        console.log("END");
+      })
+      .catch((error) => {
+        console.dir(`Error: ${error}`);
+      });
   }
 }
 
