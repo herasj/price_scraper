3d6ddd749eeab7a825ee24520e70f529c6eb88e4
Juan Rambal <herasj@uninorte.edu.co>
2020-06-28 23:28:00 -0500

    feat(mongoose): model with interface and new propierties

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,12 +1,14 @@
+import { IMongoProduct } from "../interfaces/product.interface";
 import { Schema, model } from "mongoose";
+
 const ProductSchema = new Schema(
   {
     name: { type: String, required: true },
     url: { type: String, required: true },
-    price: { type: String, required: true },
-    dcto: { type: String, required: true },
+    oldPrice: { type: String, required: true },
+    newPrice: { type: String, required: true },
   },
   { id: false, timestamps: true, versionKey: false }
 );
 
-export const ProductModel = model('Products',ProductSchema)
+export const ProductModel = model<IMongoProduct>('Products',ProductSchema)
